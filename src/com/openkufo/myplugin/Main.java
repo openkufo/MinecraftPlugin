ad1e70b27789604d5f6a0cb1ff66480e3b54706a
openkufo <openkufo@gmail.com>
2021-01-14 00:21:13 +0900

    크리퍼 폭발시 블록 파괴 방지

---
+++ src/com/openkufo/myplugin/Main.java
@@ -0,0 +1,26 @@
+package com.openkufo.myplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class Main extends JavaPlugin {
+	
+	ConsoleCommandSender consol = Bukkit.getConsoleSender();
+	
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(new CreeperGuard(), this);
+		
+		consol.sendMessage( ChatColor.AQUA + "[플러그인 활성화 중 입니다.]");
+//		getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	@Override
+	public void onDisable() {
+		consol.sendMessage( ChatColor.AQUA + "[플러그인 비활성화 중 입니다.]");
+	}
+	
+}
